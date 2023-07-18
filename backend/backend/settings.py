aae20cb5de381998774c9404c40762f7df693c99
Mango-Juicy <mango.fresh.ops@gmail.com>
2023-07-18 20:32:45 +0200

    Vercel 3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ytbj$bdrxa$dfp595#+30opfmb-g1b2gg_=wnjlh7_3ax^l)#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #VERCEL
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
